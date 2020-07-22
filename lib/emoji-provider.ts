25138033a650f19554950e58b1eb6414ee1f53f8
Avner Maiberg <avnermaiberg@gmail.com>
2020-07-22 18:43:49 -0400

    remove support for markdown emojis, convert to ts class

--- lib/emoji-provider.ts
+++ lib/emoji-provider.ts
@@ -2,17 +2,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import fuzzaldrin from 'fuzzaldrin';
-import emoji from 'emoji-images';
 import { Result } from './index';
 import CodeMirror from 'codemirror';
 
 
 export class EmojiProvider {
 	wordRegex = /::?[\w\d_\+-]+$/;
-
-	//@ts-ignore
-	packagePath = inkdrop.packages.getActivePackage('inkdrop-emojisense').path;;
-	emojiFolder = this.packagePath + '/node_modules/emoji-images/pngs';
 	properties = {};
 	keys = [];
 
@@ -20,9 +15,6 @@ export class EmojiProvider {
 		return fs.readFile(path.resolve(__dirname, '..', 'properties.json'), (error: any, content: any) => {
 			if (error) { return; }
 			this.properties = JSON.parse(content);
-			//@ts-ignore
-			this.packagePath = inkdrop.packages.getActivePackage('inkdrop-emojisense').path;
-			console.log('packagePath: ' + this.packagePath);
 			return this.keys = Object.keys(this.properties);
 		});
 	};
@@ -30,19 +22,15 @@ export class EmojiProvider {
 	getSuggestions(editor: CodeMirror.Editor): Result[] {
 		let replacementPrefix: string;
 		let prefix = this.getPrefix(editor);
-		if (!((prefix != null ? prefix.length : Number) >= 2)) { return []; }
+		if (!((prefix != null ? prefix.length : Number) >= 2)) { return []; };
 
 		if (prefix.charAt(1) === ':') {
-			const isMarkdownEmojiOnly = true;
 			replacementPrefix = prefix;
 			prefix = prefix.slice(1);
 		}
 
 		const unicodeEmojis = this.getUnicodeEmojiSuggestions(prefix);
-
-		const markdownEmojis = this.getMarkdownEmojiSuggestions(prefix, replacementPrefix);
-
-		return unicodeEmojis.concat(markdownEmojis);
+		return unicodeEmojis;
 	};
 
 	getPrefix(cm: CodeMirror.Editor): string {
@@ -62,26 +50,5 @@ export class EmojiProvider {
 				replacementPrefix: prefix,
 				rightLabel: word
 			}));
-	};
-
-	getMarkdownEmojiSuggestions(prefix: string, replacementPrefix: string): Result[] {
-		const words = fuzzaldrin.filter(emoji.list, prefix);
-		return (() => {
-			const result = [];
-			for (let word of Array.from(words)) {
-				let emojiImageElement = emoji(word, this.emojiFolder, 20);
-				if (emojiImageElement.match(/src="(.*\.png)"/)) {
-					const uri = RegExp.$1;
-					emojiImageElement = emojiImageElement.replace(uri, decodeURIComponent(uri));
-				}
-
-				result.push({
-					text: emojiImageElement,
-					replacementPrefix: replacementPrefix || prefix,
-					rightLabel: emojiImageElement
-				});
-			}
-			return result;
-		})();
 	};
 }
