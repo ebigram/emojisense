425f181c092cf3eff57e1197323660002ae291d9
Avner Maiberg <avnermaiberg@gmail.com>
2020-09-16 23:09:59 -0400

    stabilized build (WIP)

--- src/EmojiProvider.js
+++ src/EmojiProvider.js
@@ -1,76 +1,52 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmojiProvider = void 0;
-const fs = __importStar(require("fs"));
-const path = __importStar(require("path"));
-const fuzzaldrin = __importStar(require("fuzzaldrin"));
-class EmojiProvider {
-    constructor() {
-        this.wordRegex = /::?[\w\d_\+-]+$/;
-        this.properties = {};
-        this.keys = [];
-    }
-    loadProperties() {
-        return fs.readFile(path.resolve(__dirname, '..', 'properties.json'), (error, content) => {
-            if (error) {
-                return;
-            }
-            this.properties = JSON.parse(content);
-            return this.keys = Object.keys(this.properties);
-        });
-    }
-    ;
-    getSuggestions(editor) {
-        let replacementPrefix;
-        let prefix = this.getPrefix(editor);
-        if (!((prefix != null ? prefix.length : Number) >= 2)) {
-            return [];
-        }
-        ;
-        if (prefix.charAt(1) === ':') {
-            replacementPrefix = prefix;
-            prefix = prefix.slice(1);
-        }
-        const unicodeEmojis = this.getUnicodeEmojiSuggestions(prefix);
-        return unicodeEmojis;
-    }
-    ;
-    getPrefix(cm) {
-        const cursor = cm.getCursor();
-        const token = cm.getTokenAt(cursor, true);
-        const currentWord = `:${token.string}`; //prepend ':'
-        const regexMatch = currentWord.match(this.wordRegex);
-        const match = regexMatch ? regexMatch[0] : ':';
-        return match;
-    }
-    ;
-    getUnicodeEmojiSuggestions(prefix) {
-        const words = fuzzaldrin.filter(this.keys, prefix.slice(1));
-        return Array.from(words).map((word) => ({
-            text: this.properties[word].emoji,
-            replacementPrefix: prefix,
-            rightLabel: word
-        }));
-    }
-    ;
+import * as fs from 'fs'
+import * as path from 'path'
+import * as fuzzaldrin from 'fuzzaldrin'
+
+
+export class EmojiProvider {
+	wordRegex = /::?[\w\d_\+-]+$/
+	properties = {}
+	keys = []
+
+	loadProperties() {
+		return fs.readFile(path.resolve(__dirname, '..', 'properties.json'), (error, content) => {
+			if (error) { return }
+			this.properties = JSON.parse(content)
+			return this.keys = Object.keys(this.properties)
+		})
+	}
+
+	getSuggestions(editor){
+		let replacementPrefix
+		let prefix = this.getPrefix(editor)
+		if (!((prefix != null ? prefix.length : Number) >= 2)) { return [] }
+
+		if (prefix.charAt(1) === ':') {
+			replacementPrefix = prefix
+			prefix = prefix.slice(1)
+		}
+
+		const unicodeEmojis = this.getUnicodeEmojiSuggestions(prefix)
+		return unicodeEmojis
+	}
+
+	getPrefix(cm){
+		const cursor = cm.getCursor()
+		const token = cm.getTokenAt(cursor, true)
+		const currentWord = `:${token.string}` //prepend ':'
+		const regexMatch = currentWord.match(this.wordRegex)
+		const match = regexMatch ? regexMatch[0] : ':'
+		return match
+	}
+
+	getUnicodeEmojiSuggestions(prefix){
+		console.log("keys " + this.keys)
+		const words = fuzzaldrin.filter(this.keys, prefix.slice(1))
+		return Array.from(words).map((word) => (
+			{
+				text: this.properties[word].emoji,
+				replacementPrefix: prefix,
+				rightLabel: word
+			}))
+	}
 }
-exports.EmojiProvider = EmojiProvider;
--- src/emojisense.js
+++ src/emojisense.js
@@ -1,107 +1,108 @@
+// @ts-nocheck
 'use babel'
 const CodeMirror = require('codemirror')
+import { CompositeDisposable, Disposable } from 'event-kit'
 const app = require('electron').remote.app
 const modulePath = app.getAppPath() + '/node_modules/'
-require(`${modulePath}codemirror/addon/hint/show-hint`)
-require('codemirror/addon/hint/show-hint')
+require(modulePath + 'codemirror/addon/hint/show-hint')
 const { EmojiProvider } = require('./EmojiProvider')
-module.exports = {
+const R = require('ramda')
+class EmojiSense {
+    constructor() {
+        this.ep = new EmojiProvider()
+
+    }
 
 
-/**
- * @param {CodeMirror.Position} textCursor
- * @returns {CodeMirror.Hint[]}
- * @param {any[]} list
- */
-createHints(list, textCursor) {
-    const /**
-         * @param {{ text: any rightLabel: any }} elem
-         * @param {any} idx
-         * @param {any} arr
-         */
- hints = list.map((elem, idx, arr) => ({
-
-        text: elem.text,
-        displayText: `${elem.text} ${elem.rightLabel}`,
-        /**@param {HTMLLIElement} e 
-        @param {CodeMirror.Hint} cur */
-        //render: (e, data, cur) => { e.appendChild(cur.) }
-        //from?: Position
-        /** Called if a completion is picked. If provided *you* are responsible for applying the completion */
-        /**@param {CodeMirror.Editor} cm 
-         * @param {CodeMirror.Hints} data
-         * @param {CodeMirror.Hint} cur
-        */
-        hint: (cm, data, cur) => {
-            /**@type {CodeMirror.Token} token*/
-            const token = cm.getTokenAt(textCursor)
-            const line = textCursor.line
-            cm.getDoc().replaceRange(cur.text, { line: line, ch: token.start - 1}, { line: line, ch: token.end })
-        }
-        //render?: (element: HTMLLIElement, data: Hints, cur: Hint) => void
-        //to?: Position
-    }))
-    return hints
-}
-    ,
     activate() {
-        // @ts-ignore
-        global.inkdrop.onEditorLoad(this.handleEditorDidLoad.bind(this))
-    },
+        this.ep.loadProperties()
+        console.log("activate")
+        const editoHandler = R.curry(this.handleEditorDidLoad)(this.ep)
+        global.inkdrop.onEditorLoad(editoHandler.bind(this))
+    }
 
-    deactivate: function () {
+    deactivate() {
         /** @type {CodeMirror.Editor} editor **/
-        const editor = global.inkdrop.getActiveEditor()
-        if (editor) {
-            editor.off(':', () => {
-            })
+        const editor = inkdrop.getActiveEditor()
+        if (this.disposables) {
+            this.disposables.dispose()
         }
-    },
+    }
 
-
-    handleComplete(ep, cm) {
+    /**
+     * @param {{ cm: CodeMirror.Editor }} editor
+     */
+    handleEditorDidLoad(ep, editor) {
+        const { cm } = editor
         const cursor = cm.getCursor()
         const token = cm.getTokenAt(cursor)
         const start = token.start
         const end = cursor.ch
         const line = cursor.line
-
-        const results = ep.getSuggestions(cm)
-        return {
-            list: results.length ? this.createHints(results, cursor) : [],
-            from: CodeMirror.Pos(line, start),
-            to: CodeMirror.Pos(line, end),
-        }
-    },
-    /**
-     * @param {{ cm: CodeMirror.Editor }} editor
-     */
-    handleEditorDidLoad(editor) {
-        /** @type {CodeMirror.Editor} cm */
-        const cm = editor.cm
-        const ep = new EmojiProvider()
-        ep.loadProperties()
-
-        cm.on("keyup",
-            /** @param {KeyboardEvent} event **/
-            function (cm, event) {
-                if (!cm.state.completionActive && /*Enables keyboard navigation in autocomplete list*/
-                    event.key == ':' &&
-                    event.keyCode != 13) {
-                    /*Enter - do not open autocomplete list just after item has been selected in it*/
-                    // @ts-ignore
-                    inkdrop.commands.dispatch(document.body, "user:autocomplete")
-                    event.stopPropagation()
-                }
-            })
-
-        // @ts-ignore
-        inkdrop.commands.add(document.body, {
-            'user:autocomplete': () => {
-                CodeMirror.showHint(cm, ()=> this.handleComplete(ep,cm))
+        const disposables = new CompositeDisposable()
+        console.log("editor loaded")
+        const handleCompletor = R.curry(this.handleComplete)(ep)
+        cm.on("keyup", function (cm, event) {
+            //event.stopPropogation()
+            console.log("keyup!")
+            if (!cm.state.completionActive && /*Enables keyboard navigation in autocomplete list*/
+                event.key === ':' &&
+                event.keyCode != 13) {
+                /*Enables keyboard navigation in autocomplete list*/
+                //  disposables.add(new Disposable(this.handleComplete))
+                codeMirror.showHint({ hint: handleCompletor })
             }
         }
         )
+
+        this.disposables = disposables
     }
 
+    handleComplete(ep, cm) {
+        /**
+         * @param {CodeMirror.Position} textCursor
+         * @returns {CodeMirror.Hint[]}
+         * @param {any[]} list
+         */
+        function createHints(list, textCursor) {
+            const hints = list.map((elem, idx, arr) => ({
+
+                text: elem.text,
+                displayText: `${elem.text} ${elem.rightLabel}`,
+                /**@param {HTMLLIElement} e 
+                @param {CodeMirror.Hint} cur */
+                //render: (e, data, cur) => { e.appendChild(cur.) }
+                //from?: Position
+                /** Called if a completion is picked. If provided *you* are responsible for applying the completion */
+                /**@param {CodeMirror.Editor} cm 
+                 * @param {CodeMirror.Hints} data
+                 * @param {CodeMirror.Hint} cur
+                */
+                hint: (cm, data, cur) => {
+                    /**@type {CodeMirror.Token} token*/
+                    const token = cm.getTokenAt(textCursor)
+                    const line = textCursor.line
+                    cm.getDoc().replaceRange(cur.text, { line: line, ch: token.start - 1 }, { line: line, ch: token.end })
+                }
+                //render?: (element: HTMLLIElement, data: Hints, cur: Hint) => void
+                //to?: Position
+            }))
+            return hints
+        }
+
+        const cursor = cm.getCursor()
+        const token = cm.getTokenAt(cursor)
+        const start = token.start
+        const end = cursor.ch
+        const line = cursor.line
+        console.log("complete")
+        const results = ep.getSuggestions(cm)
+        console.log(results)
+        return {
+            list: results.length ? createHints(results, cursor) : ['something1', 'something2'],
+            from: CodeMirror.Pos(line, start),
+            to: CodeMirror.Pos(line, end),
+        }
+    }
 }
+module.exports = new EmojiSense()
