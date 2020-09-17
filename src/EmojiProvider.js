b630b7c9b75b46f473ed7907b892370fbb1c5c08
Avner Maiberg <avnermaiberg@gmail.com>
2020-09-17 04:10:08 -0400

    Various stablity improvemnts, bugfixes, performanc enhancements.

--- src/EmojiProvider.js
+++ src/EmojiProvider.js
@@ -1,52 +1,56 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import * as fuzzaldrin from 'fuzzaldrin'
-
+import * as fs from "fs";
+import * as path from "path";
+import * as fuzzaldrin from "fuzzaldrin";
 
 export class EmojiProvider {
-	wordRegex = /::?[\w\d_\+-]+$/
-	properties = {}
-	keys = []
+  wordRegex = /::?[\w\d_\+-]+$/;
+  properties = {};
+  keys = [];
 
-	loadProperties() {
-		return fs.readFile(path.resolve(__dirname, '..', 'properties.json'), (error, content) => {
-			if (error) { return }
-			this.properties = JSON.parse(content)
-			return this.keys = Object.keys(this.properties)
-		})
-	}
+  loadProperties() {
+    return fs.readFile(
+      path.resolve(__dirname, "..", "properties.json"),
+      (error, content) => {
+        if (error) {
+          return;
+        }
+        this.properties = JSON.parse(content);
+        return (this.keys = Object.keys(this.properties));
+      }
+    );
+  }
 
-	getSuggestions(editor){
-		let replacementPrefix
-		let prefix = this.getPrefix(editor)
-		if (!((prefix != null ? prefix.length : Number) >= 2)) { return [] }
+  getSuggestions(editor) {
+    let replacementPrefix;
+    let prefix = this.getPrefix(editor);
+    if (!((prefix != null ? prefix.length : Number) >= 2)) {
+      return [];
+    }
 
-		if (prefix.charAt(1) === ':') {
-			replacementPrefix = prefix
-			prefix = prefix.slice(1)
-		}
+    if (prefix.charAt(1) === ":") {
+      replacementPrefix = prefix;
+      prefix = prefix.slice(1);
+    }
 
-		const unicodeEmojis = this.getUnicodeEmojiSuggestions(prefix)
-		return unicodeEmojis
-	}
+    const unicodeEmojis = this.getUnicodeEmojiSuggestions(prefix);
+    return unicodeEmojis;
+  }
 
-	getPrefix(cm){
-		const cursor = cm.getCursor()
-		const token = cm.getTokenAt(cursor, true)
-		const currentWord = `:${token.string}` //prepend ':'
-		const regexMatch = currentWord.match(this.wordRegex)
-		const match = regexMatch ? regexMatch[0] : ':'
-		return match
-	}
+  getPrefix(cm) {
+    const cursor = cm.getCursor();
+    const token = cm.getTokenAt(cursor, true);
+    const currentWord = `:${token.string}`; //prepend ':'
+    const regexMatch = currentWord.match(this.wordRegex);
+    const match = regexMatch ? regexMatch[0] : ":";
+    return match;
+  }
 
-	getUnicodeEmojiSuggestions(prefix){
-		console.log("keys " + this.keys)
-		const words = fuzzaldrin.filter(this.keys, prefix.slice(1))
-		return Array.from(words).map((word) => (
-			{
-				text: this.properties[word].emoji,
-				replacementPrefix: prefix,
-				rightLabel: word
-			}))
-	}
+  getUnicodeEmojiSuggestions(prefix) {
+    const words = fuzzaldrin.filter(this.keys, prefix.slice(1));
+    return Array.from(words).map((word) => ({
+      text: this.properties[word].emoji,
+      replacementPrefix: prefix,
+      rightLabel: word,
+    }));
+  }
 }
--- src/emojisense.js
+++ src/emojisense.js
@@ -1,108 +1,111 @@
-// @ts-nocheck
-'use babel'
-const CodeMirror = require('codemirror')
-import { CompositeDisposable, Disposable } from 'event-kit'
-const app = require('electron').remote.app
-const modulePath = app.getAppPath() + '/node_modules/'
-require(modulePath + 'codemirror/addon/hint/show-hint')
-const { EmojiProvider } = require('./EmojiProvider')
-const R = require('ramda')
+"use babel";
+const CodeMirror = require("codemirror");
+import { CompositeDisposable, Disposable } from "event-kit";
+const app = require("electron").remote.app;
+const modulePath = app.getAppPath() + "/node_modules/";
+require(modulePath + "codemirror/addon/hint/show-hint");
+//require("codemirror/addon/hint/show-hint")
+const { EmojiProvider } = require("./EmojiProvider");
+const R = require("ramda");
 class EmojiSense {
-    constructor() {
-        this.ep = new EmojiProvider()
+  constructor() {
+    this.ep = new EmojiProvider();
+  }
 
+  activate() {
+    this.ep.loadProperties();
+    this.disposables = new CompositeDisposable();
+    //console.log("activate")
+    const editoHandler = R.curry(this.handleEditorDidLoad)(this.ep);
+    global.inkdrop.onEditorLoad(editoHandler.bind(this));
+  }
+
+  deactivate() {
+    /** @type {CodeMirror.Editor} editor **/
+    const editor = inkdrop.getActiveEditor();
+    if (this.disposables) {
+      this.disposables.dispose();
     }
+  }
 
+  /**
+   * @param {{ cm: CodeMirror.Editor }} editor
+   */
+  handleEditorDidLoad(ep, editor) {
+    const { cm } = editor;
+    //console.log("editor loaded")
+    const handleCompletor = R.curry(this.handleComplete)(ep);
+    cm.on("keyup", (cmEditor, event) => {
+      if (
+        !cmEditor.state.completionActive &&
+        ((event.keyCode > 37 && event.keyCode < 91) ||
+          event.keyCode ===
+            186) /*Enables keyboard navigation in autocomplete list*/
+      ) {
+        const cursor = cmEditor.getCursor();
+        const token = cmEditor.getTokenAt(cursor);
+        /*Enter - do not open autocomplete list just after item has been selected in it*/
 
-    activate() {
-        this.ep.loadProperties()
-        console.log("activate")
-        const editoHandler = R.curry(this.handleEditorDidLoad)(this.ep)
-        global.inkdrop.onEditorLoad(editoHandler.bind(this))
-    }
+        console.log(token.string);
+        if (token.string.startsWith(":"))
+          codeMirror.showHint({
+            cmEditor,
+            hint: handleCompletor,
+            completeSingle: false,
+            //closeCharacters: /\ \/>/
+          });
+      } else return CodeMirror.Pass;
+    });
+  }
 
-    deactivate() {
-        /** @type {CodeMirror.Editor} editor **/
-        const editor = inkdrop.getActiveEditor()
-        if (this.disposables) {
-            this.disposables.dispose()
-        }
-    }
-
+  handleComplete(ep, cm) {
     /**
-     * @param {{ cm: CodeMirror.Editor }} editor
+     * @param {CodeMirror.Position} textCursor
+     * @returns {CodeMirror.Hint[]}
+     * @param {any[]} list
      */
-    handleEditorDidLoad(ep, editor) {
-        const { cm } = editor
-        const cursor = cm.getCursor()
-        const token = cm.getTokenAt(cursor)
-        const start = token.start
-        const end = cursor.ch
-        const line = cursor.line
-        const disposables = new CompositeDisposable()
-        console.log("editor loaded")
-        const handleCompletor = R.curry(this.handleComplete)(ep)
-        cm.on("keyup", function (cm, event) {
-            //event.stopPropogation()
-            console.log("keyup!")
-            if (!cm.state.completionActive && /*Enables keyboard navigation in autocomplete list*/
-                event.key === ':' &&
-                event.keyCode != 13) {
-                /*Enables keyboard navigation in autocomplete list*/
-                //  disposables.add(new Disposable(this.handleComplete))
-                codeMirror.showHint({ hint: handleCompletor })
-            }
-        }
-        )
-
-        this.disposables = disposables
-    }
-
-    handleComplete(ep, cm) {
-        /**
-         * @param {CodeMirror.Position} textCursor
-         * @returns {CodeMirror.Hint[]}
-         * @param {any[]} list
+    function createHints(list, textCursor) {
+      const hints = list.map((elem, idx, arr) => ({
+        text: elem.text,
+        displayText: `${elem.text} ${elem.rightLabel}`,
+        /**@param {HTMLLIElement} e 
+                        @param {CodeMirror.Hint} cur */
+        //render: (e, data, cur) => { e.appendChild(cur.) }
+        //from?: Position
+        /** Called if a completion is picked. If provided *you* are responsible for applying the completion */
+        /**@param {CodeMirror.Editor} cm
+         * @param {CodeMirror.Hints} data
+         * @param {CodeMirror.Hint} cur
          */
-        function createHints(list, textCursor) {
-            const hints = list.map((elem, idx, arr) => ({
-
-                text: elem.text,
-                displayText: `${elem.text} ${elem.rightLabel}`,
-                /**@param {HTMLLIElement} e 
-                @param {CodeMirror.Hint} cur */
-                //render: (e, data, cur) => { e.appendChild(cur.) }
-                //from?: Position
-                /** Called if a completion is picked. If provided *you* are responsible for applying the completion */
-                /**@param {CodeMirror.Editor} cm 
-                 * @param {CodeMirror.Hints} data
-                 * @param {CodeMirror.Hint} cur
-                */
-                hint: (cm, data, cur) => {
-                    /**@type {CodeMirror.Token} token*/
-                    const token = cm.getTokenAt(textCursor)
-                    const line = textCursor.line
-                    cm.getDoc().replaceRange(cur.text, { line: line, ch: token.start - 1 }, { line: line, ch: token.end })
-                }
-                //render?: (element: HTMLLIElement, data: Hints, cur: Hint) => void
-                //to?: Position
-            }))
-            return hints
-        }
-
-        const cursor = cm.getCursor()
-        const token = cm.getTokenAt(cursor)
-        const start = token.start
-        const end = cursor.ch
-        const line = cursor.line
-        console.log("complete")
-        const results = ep.getSuggestions(cm)
-        console.log(results)
-        return {
-            list: results.length ? createHints(results, cursor) : ['something1', 'something2'],
-            from: CodeMirror.Pos(line, start),
-            to: CodeMirror.Pos(line, end),
-        }
+        hint: (cm, data, cur) => {
+          /**@type {CodeMirror.Token} token*/
+          const token = cm.getTokenAt(textCursor);
+          const line = textCursor.line;
+          cm.getDoc().replaceRange(
+            cur.text,
+            { line: line, ch: token.start - 1 },
+            { line: line, ch: token.end }
+          );
+        },
+        //render?: (element: HTMLLIElement, data: Hints, cur: Hint) => void
+        //to?: Position
+      }));
+      return hints;
     }
+
+    const cursor = cm.getCursor();
+    const token = cm.getTokenAt(cursor);
+    const start = token.start;
+    const end = cursor.ch;
+    const line = cursor.line;
+    //console.log("complete")
+    const results = ep.getSuggestions(cm);
+    //console.log(results)
+    return {
+      list: results.length ? createHints(results, cursor) : [],
+      from: CodeMirror.Pos(line, start),
+      to: CodeMirror.Pos(line, end),
+    };
+  }
 }
-module.exports = new EmojiSense()
+module.exports = new EmojiSense();
